4dd4665512684d16b11ba92af14f65c2426ca3b4
Vadim Shershen <vadim.shershen.97@gmail.com>
2023-09-26 10:14:47 +0300

    add solution without brackets

--- calculator.js
+++ calculator.js
@@ -1,11 +1,43 @@
 function calculator(expression) {
-  const priorities = {
-      '+': 1,
-      '-': 1,
-      '*': 2,
-      '/': 2,
-      '**': 3
-  };
+  expression = expression.replace(/\s/g, '');
+  const stack = [];
+  let num = '';
+  let sign = '+';
+  let result = 0;
+
+  for (let i = 0; i < expression.length; i++) {
+    const isDigit = (+expression[i] >= 0 && +expression[i] <= 9 || expression[i] === '.');
+    if (isDigit) {
+      num += expression[i];
+    } 
+    if (!isDigit || (i === expression.length - 1)) {
+      let prevNumber;
+      switch (sign) {
+        case '+':
+          stack.push(+num);
+          break;
+        case '-':
+          stack.push(num * -1);
+          break;
+        case '*':
+          prevNumber = stack.pop();
+          stack.push(prevNumber * num);
+          break;
+        case '/':
+          prevNumber = stack.pop();
+          stack.push(prevNumber / num);
+          break;
+      }
+      num = '';
+      sign = expression[i];
+    }
+  }
+
+  while (stack.length > 0) {
+    result += stack.pop();
+  }
+
+  return +result.toFixed(2);
 }
 
 module.exports = calculator;
--- calculator.test.js
+++ calculator.test.js
@@ -8,16 +8,22 @@ describe('Калькулятор', () => {
     expect(calculator('2 - 10')).toBe(-8);
   });
   test('Умножение', () => {
-    expect(calculator('5 * 4.8')).toBe(24);
+    expect(calculator('5 * 4')).toBe(20);
   });
   test('Деление', () => {
     expect(calculator('100 / 2')).toBe(50);
   });
-  test('Возведение в степень', () => {
-    expect(calculator('4 + 3 ** 2')).toBe(13);
+  test('Нецелые числа', () => {
+    expect(calculator('5.3 + 4.5 - 2.1')).toBe(7.7);
   });
   test('Несколько математических операций', () => {
     expect(calculator('2 + 3 * 4')).toBe(14);
+  });
+  test('Несколько математических операций', () => {
+    expect(calculator('2 * 3 + 10 / 2 - 1.1 + 5')).toBe(14.9);
+  });
+  test('Несколько математических операций со скобками', () => {
+    expect(calculator('(2 + 3) * 4')).toBe(20);
   });
   test('Несколько математических операций со скобками', () => {
     expect(calculator('(10 + 20 * (4 - 1)) / 2')).toBe(35);
