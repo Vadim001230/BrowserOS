db7b6f9100156d801be9089797297bf0ff89dfad
Vadim Shershen <vadim.shershen.97@gmail.com>
2023-09-26 17:32:39 +0300

    add solution LBKN-69

--- calculator.js
+++ calculator.js
@@ -1,16 +1,37 @@
 function calculator(expression) {
   expression = expression.replace(/\s/g, '');
+  return calculate(expression, 0);
+}
+
+function calculate(expression, index) {
   const stack = [];
   let num = '';
   let sign = '+';
   let result = 0;
 
-  for (let i = 0; i < expression.length; i++) {
+  for (let i = index; i < expression.length; i++) {
     const isDigit = (+expression[i] >= 0 && +expression[i] <= 9 || expression[i] === '.');
     if (isDigit) {
       num += expression[i];
     } 
     if (!isDigit || (i === expression.length - 1)) {
+      if (expression[i] === '(') {
+        num = calculate(expression, i + 1);
+        let countOpenBrackets = 1;
+        let countCloseBrackets = 0;
+        for (let j = i + 1; j < expression.length; j++) {
+          if (expression[j] === ')') {
+            countCloseBrackets++;
+            if (countOpenBrackets === countCloseBrackets) {
+              i = j + 1;
+              break;
+            }
+          } else if (expression[j] === '(') {
+            countOpenBrackets++;
+          }
+        }
+      }
+
       let prevNumber;
       switch (sign) {
         case '+':
@@ -30,14 +51,14 @@ function calculator(expression) {
       }
       num = '';
       sign = expression[i];
+      if (expression[i] === ')') break;
     }
   }
 
   while (stack.length > 0) {
     result += stack.pop();
   }
-
-  return +result.toFixed(2);
+  return result;
 }
 
 module.exports = calculator;
--- calculator.test.js
+++ calculator.test.js
@@ -14,13 +14,13 @@ describe('Калькулятор', () => {
     expect(calculator('100 / 2')).toBe(50);
   });
   test('Нецелые числа', () => {
-    expect(calculator('5.3 + 4.5 - 2.1')).toBe(7.7);
+    expect(calculator('5.3 + 4.5 - 2.1')).toBeCloseTo(7.7, 2);
   });
   test('Несколько математических операций', () => {
     expect(calculator('2 + 3 * 4')).toBe(14);
   });
   test('Несколько математических операций', () => {
-    expect(calculator('2 * 3 + 10 / 2 - 1.1 + 5')).toBe(14.9);
+    expect(calculator('2 * 3 + 10 / 2 - 1.1 + 5')).toBeCloseTo(14.9, 2);
   });
   test('Несколько математических операций со скобками', () => {
     expect(calculator('(2 + 3) * 4')).toBe(20);
