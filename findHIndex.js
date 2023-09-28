75963c3fe1cd3bbb8fcfb6a1acd4286e66c1fa86
Vadim Shershen <vadim.shershen.97@gmail.com>
2023-09-28 16:27:41 +0300

    add solution LBKN-12

---
+++ findHIndex.js
@@ -0,0 +1,12 @@
+function findHIndex(citations) {
+  citations.sort((a, b) => b - a);
+
+  for (let i = 0; i < citations.length; i++) {
+    if (citations[i] <= i) {
+      return i;
+    }
+  }
+  
+  return citations.length;
+}
+module.exports = findHIndex;
