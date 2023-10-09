72341b002bddac45a712e7b27988722378b34b75
Vadim Shershen <vadim.shershen.97@gmail.com>
2023-10-09 09:42:00 +0300

    refactor: add tests description

--- findHIndex.test.js
+++ findHIndex.test.js
@@ -1,22 +1,13 @@
 const findHIndex = require('./findHIndex');
 
 describe('Найти h-index', () => {
-  test('Тест 1', () => {
+  test('Несколько публикаций по одной цитате', () => {
     expect(findHIndex([1, 1, 1])).toBe(1);
   });
-  test('Тест 2', () => {
-    expect(findHIndex([1, 3, 2])).toBe(2);
-  });
-  test('Тест 3', () => {
-    expect(findHIndex([1, 3, 2])).toBe(2);
-  });
-  test('Тест 4', () => {
+  test('Одна публикация', () => {
     expect(findHIndex([100])).toBe(1);
   });
-  test('Тест 5', () => {
+  test('Несколько публикаций с разным количеством цитат', () => {
     expect(findHIndex([8, 10, 12, 5, 4, 2])).toBe(4);
-  });
-  test('Тест 6', () => {
-    expect(findHIndex([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBe(5);
   });
 });
