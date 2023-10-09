87dd19dc0b029b197325f3b2be7ae2bf332ed503
Vadim Shershen <vadim.shershen.97@gmail.com>
2023-10-09 10:00:36 +0300

    refactor: fix tests

--- findUniqueValues.test.js
+++ findUniqueValues.test.js
@@ -1,11 +1,9 @@
 const findUniqueValues = require('./findUniqueValues');
 
 describe('Поиск уникальных значений', () => {
-  test('Уникальные значения – числа', () => {
-    expect(findUniqueValues([1, 2, 3, 1, 3, 1, 2])).toEqual([1, 2, 3]);
-  });
-  test('Уникальные значения – строки', () => {
-    expect(findUniqueValues(['Да', 'Нет', 'Наверное', 'Да', 'Нет'])).toEqual(['Да', 'Нет', 'Наверное']);
+  test('Разный тип данных', () => {
+    expect(findUniqueValues([1, 2, 3, 1, 3, 1, 2, 'Да', 'Нет', 'Наверное', 'Да', 'Нет', false, false]))
+      .toEqual([1, 2, 3, 'Да', 'Нет', 'Наверное', false]);
   });
   test('Пустой массив', () => {
     expect(findUniqueValues([])).toEqual([]);
