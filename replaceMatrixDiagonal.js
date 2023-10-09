9c51ab0520ee1013711321f1fa2600d379663638
Vadim Shershen <vadim.shershen.97@gmail.com>
2023-10-09 09:46:56 +0300

    feat: add solution in one cycle for(;;)

--- replaceMatrixDiagonal.js
+++ replaceMatrixDiagonal.js
@@ -1,10 +1,6 @@
 function replaceMatrixDiagonal(matrix) {
   for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      if (i === j) {
-        matrix[i][j] < 0 ? matrix[i][j] = 0 : matrix[i][j] = 1;
-      }
-    }
+    matrix[i][i] < 0 ? matrix[i][i] = 0 : matrix[i][i] = 1;
   }
   return matrix;
 }
