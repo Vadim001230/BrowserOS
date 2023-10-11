202609c7df1744b51d209c3dfa7ba169d62f45d6
Vadim Shershen <vadim.shershen.97@gmail.com>
2023-10-11 15:48:27 +0300

    delete regular expression, add better name of variables

--- tasks/LBKN_13-find-maxlength-palindrome/findMaxPalindrome.js
+++ tasks/LBKN_13-find-maxlength-palindrome/findMaxPalindrome.js
@@ -1,25 +1,25 @@
 function findMaxPalindrome(str) {
-  str = str.toLowerCase().replace(/\s/g, '');
-
+  str = str.toLowerCase();
+  const strLength = str.length;
   let longestPalindrome = str[0];
   
   function findPalindrome(left, right) {
-    while (left >= 0 && right < str.length && str[left] === str[right]) {
+    while (left >= 0 && right < strLength && str[left] === str[right]) {
       let palindrome = str.slice(left, right + 1);
       longestPalindrome = palindrome.length > longestPalindrome.length ? palindrome : longestPalindrome;
-      left--;
-      right++;
+      left = left - 1;
+      right = right + 1;
     }
   }
 
-  for (let i = 0; i < str.length; i++) {
-    let left = i - 1;
-    let right = i + 1;
-    findPalindrome(left, right);
+  for (let i = 0; i < strLength; i++) {
+    const leftFlagForOddLengthPalindrome = i - 1;
+    const rightFlagForOddLengthPalindrome = i + 1;
+    findPalindrome(leftFlagForOddLengthPalindrome, rightFlagForOddLengthPalindrome);
 
-    left = i;
-    right = i + 1;
-    findPalindrome(left, right);
+    const leftFlagForEvenLengthPalindrome = i;
+    const rightFlagForEvenLengthPalindrome = i + 1;
+    findPalindrome(leftFlagForEvenLengthPalindrome, rightFlagForEvenLengthPalindrome);
   }
 
   return longestPalindrome;
--- tasks/LBKN_13-find-maxlength-palindrome/findMaxPalindrome.test.js
+++ tasks/LBKN_13-find-maxlength-palindrome/findMaxPalindrome.test.js
@@ -1,10 +1,10 @@
 const findMaxPalindrome = require('./findMaxPalindrome');
 
 describe('Поиск наибольшего полиндрома', () => {
-  test('Проверка строки с английскими буквами', () => {
-    expect(findMaxPalindrome('sswweeeeesdfdsf')).toBe('eeeee');
+  test('Проверка строки с английскими буквами и четным количеством символов', () => {
+    expect(findMaxPalindrome('sswweeeeeesdfdsf')).toBe('eeeeee');
   });
-  test('Проверка строки с цифрами и английскими буквами', () => {
+  test('Проверка строки с цифрами, английскими буквами и нечетным количеством символов', () => {
     expect(findMaxPalindrome('fds234325iertuiejgkjjviiii2iiiiijgjdjgryy435435ghs')).toBe('iiii2iiii');
   });
 });
