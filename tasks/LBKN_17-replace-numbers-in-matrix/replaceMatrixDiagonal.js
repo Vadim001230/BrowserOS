ddaf7c5302d3be41bd3f167180295ab3124ce816
Vadim Shershen <vadim.shershen.97@gmail.com>
2023-10-11 11:29:25 +0300

    fix ternary operator

--- tasks/LBKN_17-replace-numbers-in-matrix/replaceMatrixDiagonal.js
+++ tasks/LBKN_17-replace-numbers-in-matrix/replaceMatrixDiagonal.js
@@ -1,6 +1,6 @@
 function replaceMatrixDiagonal(matrix) {
   for (let i = 0; i < matrix.length; i++) {
-    matrix[i][i] < 0 ? matrix[i][i] = 0 : matrix[i][i] = 1;
+    matrix[i][i] = matrix[i][i] < 0 ? 0 : 1;
   }
   return matrix;
 }
