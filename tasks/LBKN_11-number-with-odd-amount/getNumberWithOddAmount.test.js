47bb9280d34b33d8a266b04b8f7adad35c407935
Vadim Shershen <vadim.shershen.97@gmail.com>
2023-10-11 11:24:23 +0300

    fix tests

--- tasks/LBKN_11-number-with-odd-amount/getNumberWithOddAmount.test.js
+++ tasks/LBKN_11-number-with-odd-amount/getNumberWithOddAmount.test.js
@@ -1,17 +1,13 @@
 const getNumbersWithOddAmount = require('./getNumberWithOddAmount');
 
-const arrayToTest = [1, 1, 5, 3, 3, 5, 5];
-const arrayToTest2 = [1, 2, 3, 3, 4, -2, -2, 3, 1, 2, 4];
-const arrayToTest3 = [1, 2, 3, 1, 10, 2, 10, 3, 3, 10];
+const arrayToTest1 = [1, 1, 5, 3, 3, 5, 5];
+const arrayToTest2 = [1, 2, 3, 1, 10, 2, 10, 3, 3, 10];
 
 describe('Число с нечетным количеством', () => {
   test('Одно число в массиве', () => {
-    expect(getNumbersWithOddAmount(arrayToTest)).toEqual([5]);
-  });
-  test('Одно число в массиве', () => {
-    expect(getNumbersWithOddAmount(arrayToTest2)).toEqual([3]);
+    expect(getNumbersWithOddAmount(arrayToTest1)).toEqual([5]);
   });
   test('Несколько чисел в массиве', () => {
-    expect(getNumbersWithOddAmount(arrayToTest3)).toEqual([3, 10]);
+    expect(getNumbersWithOddAmount(arrayToTest2)).toEqual([3, 10]);
   });
 });
