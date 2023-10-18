6f000cf8a066ad8ba10d36a4dfe89d60984c079e
Vadim Shershen <vadim.shershen.97@gmail.com>
2023-10-18 13:03:47 +0300

    refactor count.js

--- tasks/LBKN_92-refactor-count/count.js
+++ tasks/LBKN_92-refactor-count/count.js
@@ -1,12 +1,10 @@
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
+function transformDay(day) {
+  return day[0].toUpperCase() + day.slice(1, 3).toLowerCase();
+}
+
 function getNumberOfDay(day) {
-  let num = -1;
-  day = day.toLowerCase();
-  days.forEach((elem, index) => {
-    if (day.startsWith(elem.toLowerCase())) {
-      num = index + 1;
-    }
-  });
-  return num;
+  const dayIndex = days.indexOf(transformDay(day));
+  return dayIndex === -1 ? dayIndex : dayIndex + 1;
 }
