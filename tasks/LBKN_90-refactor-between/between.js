269264fb6d3d2de7245b4921c70872e5e4c1f188
Vadim Shershen <vadim.shershen.97@gmail.com>
2023-10-19 09:36:04 +0300

    refactor between.js

--- tasks/LBKN_90-refactor-between/between.js
+++ tasks/LBKN_90-refactor-between/between.js
@@ -1,7 +1,17 @@
 function getValueBetween(str, prefix, suffix) {
   const prefixIndex = str.indexOf(prefix);
-  if (prefixIndex === -1) return '';
+  if (prefixIndex === -1) {
+    return '';
+  }
   str = str.substring(prefixIndex + prefix.length);
-  const suffixIndex = suffix ? str.indexOf(suffix) : str.length;
-  return suffixIndex === -1 ? '' : str.substring(0, suffixIndex);
+
+  if (suffix) {
+    const suffixIndex = str.indexOf(suffix);
+    if (suffixIndex === -1) {
+      return '';
+    }
+    str = str.substring(0, suffixIndex);
+  }
+
+  return str;
 }
