ab6d33d82090602d0b2c2a0e8d859c5232b6ba74
Vadim Shershen <vadim.shershen.97@gmail.com>
2023-10-31 10:34:21 +0300

    refactor sample.js

--- tasks/LBKN_116-refactor-sample/sample.js
+++ tasks/LBKN_116-refactor-sample/sample.js
@@ -1,6 +1,3 @@
 const getRandomNumberFromRange = require('../../utils/number');
 
-function getRandomElementFromArr(arr) {
-  const randomIndex = getRandomNumberFromRange(0, arr.length - 1);
-  return arr[randomIndex];
-}
+const getRandomElementFromArr = (arr) => arr[getRandomNumberFromRange(0, arr.length - 1)];
