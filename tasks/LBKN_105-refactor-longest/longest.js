5387bd576491df0a728bb2c38301181c667585c6
Vadim Shershen <vadim.shershen.97@gmail.com>
2023-10-31 11:53:32 +0300

    refactor longest.js

--- tasks/LBKN_105-refactor-longest/longest.js
+++ tasks/LBKN_105-refactor-longest/longest.js
@@ -1,10 +1,10 @@
-function findLongestStr(strings) {
+function findLongestStr(strings = []) {
   let longestStr = '';
   for (const str of strings) {
     if (str.length > longestStr.length) {
       longestStr = str;
     }
   }
-
+  
   return longestStr;
 }
