05b51efac74efbbaa899e81e2b7ff4aa279825b4
Vadim Shershen <vadim.shershen.97@gmail.com>
2023-11-07 14:21:09 +0300

    refactor password.js

--- tasks/LBKN_109-refactor-password/password.js
+++ tasks/LBKN_109-refactor-password/password.js
@@ -2,7 +2,7 @@ const getRandomNumberFromRange = require('../../utils/number');
 
 function generatePassword(alphabet, passLength) {
   let password = '';
-  for (let i = 0; i < passLength; i++) {
+  for (let i = passLength - 1; i >= 0; i--) {
     const randomIndex = getRandomNumberFromRange(0, alphabet.length - 1);
     password += alphabet[randomIndex];
   }
