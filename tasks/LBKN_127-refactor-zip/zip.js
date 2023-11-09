32cd68ffe88777f2858b9b58673f3352bd48f601
Vadim Shershen <vadim.shershen.97@gmail.com>
2023-11-09 11:48:10 +0300

    refactor zip.js

--- tasks/LBKN_127-refactor-zip/zip.js
+++ tasks/LBKN_127-refactor-zip/zip.js
@@ -1,10 +1,10 @@
 function zipArrays(arr1 = [], arr2 = []) {
   const result = [];
-  arr1.forEach((el, i) => {
-    if (arr2[i]) {
-      result.push([el, arr2[i]]);
-    }
-  });
+  const minLength = Math.min(arr1.length, arr2.length);
+
+  for (let i = 0; i < minLength; i++) {
+    result.push([arr1[i], arr2[i]]);
+  }
 
   return result;
 }
