8239d315a0d9c82d8fa09d60c6f0f7c051ee1e0f
Vadim Shershen <vadim.shershen.97@gmail.com>
2023-11-17 15:19:37 +0300

    create react through webpack project

---
+++ tasks/LBKN_133-create-react-project-webpack/src/components/Main.jsx
@@ -0,0 +1,11 @@
+import img from '../assets/images/my-img.webp';
+import './style.scss';
+
+export function Main() {
+  return (
+    <main className='main'>
+      <p>Main</p>
+      <img src={img} className="img" alt="img" width={400} />
+    </main>
+  );
+}
