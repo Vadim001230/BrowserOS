dc01ebe7080723fe2c4303111cca217bb79ed24b
Vadim Shershen <vadim.shershen.97@gmail.com>
2024-01-04 13:49:08 +0300

    refactor TaskBar

--- tasks/LBKN_155-browser-os/src/components/TaskBar/TaskBar.tsx
+++ tasks/LBKN_155-browser-os/src/components/TaskBar/TaskBar.tsx
@@ -1,43 +1,27 @@
 import { useState } from 'react';
 import { useAppSelector } from '@/hooks/redux';
 import { useBattery } from '@/hooks/useBattery';
-import { openShortcutService, getAppsByShortcutsList } from '@/serviсes/appServices';
+import { getAppsByShortcutsList } from '@/serviсes/appServices';
 import { IApp } from '@/types/IApp';
 import { BaseButton } from '@/components/UI/BaseButton/BaseButton';
 import { BatteryStatusButton } from '@/components/BatteryStatusButton/BatteryStatusButton';
 import { Clock } from '@/components/Clock/Clock';
 import { StartMenu } from '@/components/StartMenu/StartMenu';
 import { BatterySettingPopup } from '@/components/BatteryPopupSettings/BatterySettingPopup';
-import { TaskbarShortcutContextMenu } from '@/components/TaskbarShortcutContextMenu/TaskbarShortcutContextMenu';
 import { TaskbarAppButton } from '@/components/TaskbarAppButton/TaskbarAppButton';
 import './TaskBar.scss';
 
 export const TaskBar = () => {
-  const [isShortcutContextMenuShown, setIsShortcutContextMenuShown] = useState(false);
   const [isBatteryPopupShown, setIsBatteryPopupShown] = useState(false);
   const [isStartMenuShown, setIsStartMenuShown] = useState(false);
-  const [selectedId, setSelectedId] = useState<IApp['id']>(0);
   const [targetElement, setTargetElement] = useState<HTMLElement>();
   const batteryStatus = useBattery();
 
   const openedApps = getAppsByShortcutsList(useAppSelector((state) => state.taskbar.openedApps));
   const favoritApps =  getAppsByShortcutsList(useAppSelector((state) => state.taskbar.favoritApps));
 
-  const closeAppPopupMenu = () => setIsShortcutContextMenuShown(false);
   const closeBatteryPopup = () => setIsBatteryPopupShown(false);
   const closeStartMenu = () => setIsStartMenuShown(false);
-
-  const handleAppClick = (id: IApp['id']) => {
-    setSelectedId(id);
-    openShortcutService(id);
-  };
-
-  const handleAppContextMenu = (e: React.MouseEvent<HTMLButtonElement>, id: IApp['id']) => {
-    e.preventDefault();
-    setTargetElement(e.currentTarget);
-    setSelectedId(id);
-    setIsShortcutContextMenuShown(true);
-  };
 
   const handleStartMenuClick = () => {
     if (!isStartMenuShown) {
@@ -52,12 +36,7 @@ export const TaskBar = () => {
 
   const renderAppButtons = (apps: IApp[]) => apps.map((app) => {
     return (
-      <TaskbarAppButton
-        key={app.id}
-        onClick={() => handleAppClick(app.id)}
-        onContextMenu={(e) => handleAppContextMenu(e, app.id)}
-        app={app}
-      />
+      <TaskbarAppButton key={app.id} app={app} />
     );
   });
 
@@ -75,7 +54,6 @@ export const TaskBar = () => {
         {!!favoritApps.length && renderAppButtons(favoritApps)}
         {!!openedApps.length && renderAppButtons(openedApps.filter((app) => !favoritApps.includes(app)))}
       </div>
-      {isShortcutContextMenuShown && <TaskbarShortcutContextMenu id={selectedId} onClose={closeAppPopupMenu} target={targetElement} />}
       <div className='taskbar__container'>
         {batteryStatus && <BatteryStatusButton onClick={handleBatteryStatus} level={batteryStatus.level} charging={batteryStatus.charging} />}
         <Clock />
--- tasks/LBKN_155-browser-os/src/components/TaskbarAppButton/TaskbarAppButton.tsx
+++ tasks/LBKN_155-browser-os/src/components/TaskbarAppButton/TaskbarAppButton.tsx
@@ -1,22 +1,48 @@
+import { useState } from 'react';
 import { useAppSelector } from '@/hooks/redux';
+import { openShortcutService } from '@/serviсes/appServices';
 import { IApp } from '@/types/IApp';
 import { BaseButton, BaseButtonProps } from '@/components/UI/BaseButton/BaseButton';
+import { TaskbarShortcutContextMenu } from '@/components/TaskbarShortcutContextMenu/TaskbarShortcutContextMenu';
 
 interface Props extends Omit<BaseButtonProps, 'children'> {
   app: IApp;
 }
 
 export const TaskbarAppButton = ({ app, ...attributes }: Props) => {
-  const win = useAppSelector((state) => state.windows.windows.find((win) => win.id === app.id));
+  const [isShortcutContextMenuShown, setIsShortcutContextMenuShown] = useState(false);
+  const [selectedId, setSelectedId] = useState<IApp['id']>(0);
+  const [targetElement, setTargetElement] = useState<HTMLElement>();
   
+  const win = useAppSelector((state) => state.windows.windows.find((win) => win.id === app.id));
+
+  const closeAppPopupMenu = () => setIsShortcutContextMenuShown(false);
+  const handleAppClick = (id: IApp['id']) => {
+    setSelectedId(id);
+    openShortcutService(id);
+  };
+
+  const handleAppContextMenu = (e: React.MouseEvent<HTMLButtonElement>, id: IApp['id']) => {
+    e.preventDefault();
+    setTargetElement(e.currentTarget);
+    setSelectedId(id);
+    setIsShortcutContextMenuShown(true);
+  };
+
   return (
-    <BaseButton
-      className={`taskbar__app ${win ? 'taskbar__app_opened' : ''}`}
-      isChecked={win?.isFocused && !win?.isMinimized}
-      title={app.title}
-      {...attributes}
-    >
-      <img src={app.iconURL} alt='' />
-    </BaseButton>
+    <>
+      <BaseButton
+        className={`taskbar__app ${win ? 'taskbar__app_opened' : ''}`}
+        isChecked={win?.isFocused && !win?.isMinimized}
+        onClick={() => handleAppClick(app.id)}
+        onContextMenu={(e) => handleAppContextMenu(e, app.id)}
+        title={app.title}
+        {...attributes}
+      >
+        <img src={app.iconURL} alt='' />
+      </BaseButton>
+      {isShortcutContextMenuShown && <TaskbarShortcutContextMenu id={selectedId} onClose={closeAppPopupMenu} target={targetElement} />}
+    </>
   );
 };
+
